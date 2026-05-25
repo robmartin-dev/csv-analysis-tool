96c089c104df9f035766523415234851866c3f7a
= <robmartin-dev@proton.me>
2026-05-25 14:40:01 +0100

    fields added to buffer one by one

--- src/main.c
+++ src/main.c
@@ -25,39 +25,44 @@ int main(int argc, char **argv) {
 		}	
 
 		int ch;
+		char field_buffer[256];
+		int buf_idx = 0;
 		enum State state = STATE_UNQUOTED;
-		int new_row = 1;
-		int new_field = 1;
+		int new_row = 0;
+		int new_field = 0;
+		putc('{', stdout);
 		while ((ch = fgetc(file)) != EOF) {
+			if (state == STATE_UNQUOTED) {
+				if (ch == '"') {
+					state = STATE_QUOTED;
+				} else if (ch == ',') {
+					field_buffer[buf_idx] = '\0';
+					new_field = 1;
+				} else if (ch == '\n') {
+					field_buffer[buf_idx] = '\0';
+					putc('}', stdout);
+					new_row = 1;
+					new_field = 1;
+				} else {
+					field_buffer[buf_idx++] = ch;
+				}
+			} else {
+				if (ch == '"') {
+					state = STATE_UNQUOTED;
+				} else {
+					field_buffer[buf_idx++] = ch;
+				}
+			}
+
 			if (new_row) {
 				putc('\n', stdout);
 				putc('{', stdout);
 				new_row = 0;
 			}
 			if (new_field) {
-				putc('[', stdout);
+				printf("[%s] ", field_buffer);
+				buf_idx = 0;
 				new_field = 0;
-			}
-			if (state == STATE_UNQUOTED) {
-				if (ch == '"') {
-					state = STATE_QUOTED;
-				} else if (ch == ',') {
-					putc(']', stdout);
-					new_field = 1;
-				} else if (ch == '\n') {
-					putc(']', stdout);
-					putc('}', stdout);
-					new_row = 1;
-					new_field = 1;
-				} else {
-					putc(ch, stdout);
-				}
-			} else {
-				if (ch == '"') {
-					state = STATE_UNQUOTED;
-				} else {
-					putc(ch, stdout);
-				}
 			}
 		}
 	}
